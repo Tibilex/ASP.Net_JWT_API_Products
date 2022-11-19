e208137289a763f8f19ba4197083c4eb05ed7693
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-11-19 16:33:35 +0200

    Add: data context

---
+++ ASP.Net_JWT_API_Products/Data/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using ASP.Net_JWT_API_Products.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ASP.Net_JWT_API_Products.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DataBaseContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Login> Logins { get; set; }
+    }
+}
